5128453c2c995c2dcb73d7e7efd06c70eb2b6fd1
Patryk Krzyzanski <hannover1992@googlemail.com>
2023-10-26 03:15:28 +0200

    alles wor, start to install advence lsp

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -1,4 +1,5 @@
 return require('packer').startup(function(use)
+  
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   
@@ -13,6 +14,23 @@ return require('packer').startup(function(use)
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
   use ('nvim-treesitter/nvim-treesitter', {run  = ':TSUpdate'})
-  
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  --- Uncomment these if you want to manage LSP servers from neovim
+		  -- {'williamboman/mason.nvim'},
+		  -- {'williamboman/mason-lspconfig.nvim'},
+
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+}
+
 
 end)
