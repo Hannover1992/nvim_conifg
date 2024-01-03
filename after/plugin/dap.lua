a6b92914291b32bcabd17cd92d0e111b7ddbf109
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-01-03 15:27:35 +0100

    add the ,  possbility to using backend and front end

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -9,42 +9,44 @@ vim.api.nvim_set_keymap("n", "<Leader>do", "<cmd>DapStepOut<CR>", {noremap=true}
 vim.api.nvim_set_keymap("n", "<leader>db", ":lua require('dap').toggle_breakpoint()<CR>", {noremap=true})
 
 
---
+-- google-chrome --remote-debugging-port=9222 --user-data-dir=/path/to/new/directory
+
+
 local dap = require('dap')
 
 require('dap').set_log_level('DEBUG')
 
--- this one is for node in backend
+-- Node Adapter
 dap.adapters.node2 = {
   type = 'executable',
   command = 'node',
   args = { '/home/uczen/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' },
 }
 
-dap.configurations.typescript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${workspaceFolder}/build/index.js',  -- Adjust this path to your compiled JavaScript file
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    outFiles = { '${workspaceFolder}/build/**/*.js' },  -- Adjust this to your JavaScript output files
-    console = 'integratedTerminal',
-  },
-}
-
-
---This one is for Angular
+-- Chrome Adapter
 dap.adapters.chrome = {
   type = "executable",
   command = "node",
   args = {"/home/uczen/.local/share/nvim/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js"}
 }
 
+-- Configurations
 dap.configurations.typescript = {
+  -- Node configuration
   {
+    name = 'Launch Node',
+    type = 'node2',
+    request = 'launch',
+    program = '${workspaceFolder}/build/index.js',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    outFiles = { '${workspaceFolder}/build/**/*.js' },
+    console = 'integratedTerminal',
+  },
+  -- Chrome configuration
+  {
+    name = "Attach Chrome",
     type = "chrome",
     request = "attach",
     program = "${file}",
@@ -55,22 +57,21 @@ dap.configurations.typescript = {
   }
 }
 
-
-
-dap.adapters.python = {
-  type = 'executable';
-  command = '/home/uczen/.local/share/nvim/mason/packages/debugpy/venv/bin/python'; -- Path to the Python executable in the virtual environment
-  args = { '-m', 'debugpy.adapter' };
-}
-
-dap.configurations.python = {
-  {
-    type = 'python';  -- The type here should match the adapter name
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";  -- This should point to the file you're debugging
-    pythonPath = function()
-      return '/usr/bin/python3'  -- Again, the path to the Python executable in the virtual environment
-    end;
-  },
-}
+--
+-- dap.adapters.python = {
+--   type = 'executable';
+--   command = '/home/uczen/.local/share/nvim/mason/packages/debugpy/venv/bin/python'; -- Path to the Python executable in the virtual environment
+--   args = { '-m', 'debugpy.adapter' };
+-- }
+--
+-- dap.configurations.python = {
+--   {
+--     type = 'python';  -- The type here should match the adapter name
+--     request = 'launch';
+--     name = "Launch file";
+--     program = "${file}";  -- This should point to the file you're debugging
+--     pythonPath = function()
+--       return '/usr/bin/python3'  -- Again, the path to the Python executable in the virtual environment
+--     end;
+--   },
+-- }
