46372c3d3c4cf26097e1bfbee886fe02b182bf7c
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-05-12 10:45:56 +0200

    copilot and clean of reampt

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -15,5 +15,17 @@ end
 
 
 
+
 -- Set the keymap in normal mode
 vim.keymap.set('n', '<leader>ct', ToggleCopilot, { noremap = true, silent = true })
+
+-- Toggle Copilot and open Copilot panel with Lua in Neovim
+vim.keymap.set('n', '<leader>cp', function() vim.cmd('Copilot panel') end, { noremap = true, silent = true })
+
+-- If you'd rather use a key that isn't <Tab>, define an <expr> map that calls
+-- copilot#Accept().  Here's an example with CTRL-J:
+
+vim.keymap.set('i', '<C-j>', 'copilot#Accept()', { noremap = true, silent = true, expr = true })
+
+
+
