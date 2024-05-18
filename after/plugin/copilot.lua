b7b64dbd7d7726258e784bf8bc2154e1374b43dc
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-05-18 11:36:51 +0200

    make the strange after space go away

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,29 +1,3 @@
--- Assuming 'copilot_active' is a variable you manage to track its state
-local copilot_active = false
-
-function ToggleCopilot()
-    if copilot_active then
-        -- Disable Copilot
-        vim.cmd('Copilot disable')  -- Replace with the actual command to disable Copilot
-        copilot_active = false
-    else
-        -- Enable Copilot
-        vim.cmd('Copilot enable')  -- Replace with the actual command to enable Copilot
-        copilot_active = true
-    end
-end
-
-
-
-
--- -- fass die funktio oben zusammen
--- Setze die Keymaps für Normal- und Insert-Modus
-local modes = {'n', 'i'}
-for _, mode in ipairs(modes) do
-  vim.keymap.set(mode, '<leader>ct', ToggleCopilot, { noremap = true, silent = true })
-end
-
-
 -- Toggle Copilot and open Copilot panel with Lua in Neovim
 vim.keymap.set('n', '<leader>cp', function() vim.cmd('Copilot panel') end, { noremap = true, silent = true })
 
--- after/plugin/jdtls.lua
+++ after/plugin/jdtls.lua
@@ -1,7 +1,7 @@
-local config = {
-     autostart = true,
-    cmd = {'/home/uczen/.local/state/nvim/jdtls/bin/jdtls'},
-    -- root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-    root_dir = '/home/uczen/Uni/2Hor/Assignment_04/Auf1/',
-}
-require('jdtls').start_or_attach(config)
+-- local config = {
+--      autostart = true,
+--     cmd = {'/home/uczen/.local/state/nvim/jdtls/bin/jdtls'},
+--     -- root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+--     root_dir = '/home/uczen/Uni/2Hor/Assignment_04/Auf1/',
+-- }
+-- require('jdtls').start_or_attach(config)
